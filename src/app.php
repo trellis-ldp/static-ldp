93ab997e16468f8f3756ee162dc1cc284e798617
Aaron Coburn <acoburn@amherst.edu>
2017-04-03 08:31:01 -0400

    Remove unused Islandora reference

--- src/app.php
+++ src/app.php
@@ -6,7 +6,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Silex\Application;
 use Psr\Http\Message\ResponseInterface;
-use Islandora\Crayfish\Provider\CrayfishProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Trellis\StaticLdp\Controller\ResourceController;
